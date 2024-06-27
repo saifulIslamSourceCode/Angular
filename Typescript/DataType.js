ccf5c4f2912b3e184120cce24037111e7603e546
Saiful <76849761+saifulIslamSourceCode@users.noreply.github.com>
2024-06-27 23:37:03 +0600

    Practice TS

--- Typescript/DataType.js
+++ Typescript/DataType.js
@@ -1,12 +1,18 @@
-var fn = function () { return 'response'; };
+"use strict";
+var fn = () => 'response';
 //ts allows type declaration
 var a;
 var b;
 var c;
-//a = 10;
+//a = 10;dsf
 b = true;
 c = 'saiful';
 //var myarray = [1,3,4];
 var myarray;
 myarray = [1, 3, 5];
-a = myarray.pop();
+a = myarray[0];
+//tuple
+var ar = [1, true];
+ar = [5, false];
+//video 7 
+console.log(ar);
--- Typescript/DataType.ts
+++ Typescript/DataType.ts
@@ -1,18 +1,13 @@
-var fn = () => 'response';
-//ts allows type declaration
-var a : number;
-var b : boolean;
-var c : string;
+let x = 20;
+console.log(x);
 
-//a = 10;dsf
-b = true;
-c = 'saiful';
+for(var i = 0; i < 5; i++){
 
-//var myarray = [1,3,4];
-var myarray : number[];
-myarray = [1, 3, 5];
-a = myarray[0];
-//tuple
-var ar : [number, boolean] = [1, true];
-ar = [5, false]
-//video 7 
+}
+
+console.log(i);
+
+for(let a = 0; a < 5; a++){
+
+}
+console.log(a)
